c54ba269c2b8cb6d2e5f92d60255fa16dcd8f03d
Matyanson <matyas.perem@seznam.cz>
2022-02-05 12:52:39 +0100

    chore: made it recursive

--- src/components/ObjectEditor.svelte
+++ src/components/ObjectEditor.svelte
@@ -1,35 +1,57 @@
 <script lang="ts">
     export let obj;
 
-    const onDelete = (key: string, index: number) => {
-        obj[key] = [
-            obj[key].slice(0, index),
-            obj[key].slice(index + 1)
+    const isArray = Array.isArray(obj);
+
+    const onDelete = (index: number) => {
+        obj = [
+            ...obj.slice(0, index),
+            ...obj.slice(index + 1)
         ]
     }
-    const onAdd = (key: string) => {
-        obj[key] = [...obj[key], ''];
+    const onAdd = (type: string) => {
+        let newItem;
+        switch(type) {
+            case 'number': newItem = 0; break;
+            case 'boolean': newItem = false; break;
+            default: newItem = ''; break;
+        }
+        obj = [...obj, newItem];
     }
 </script>
 
-{#each Object.entries(obj) as [key, value]}
-    <div class="property">
-        <div class="key">{key} : </div>
+{#if obj == null}
+    <div class="null">{String(obj)}</div>
+{:else if typeof obj === "number"}
+    <input type="number" bind:value={obj} />
+{:else if typeof obj === "string"}
+    <input type="text" bind:value={obj} />
+{:else if typeof obj === "boolean"}
+    <input type="checkbox" bind:value={obj} />
+{:else if isArray}
+    <div class="collection">
+        {#each obj as item, i}
         <div class="value">
-            {#if Array.isArray(value)}
-                <div class="collection">
-                    {#each value as item, i}
-                    <input type="text" bind:value={obj[key][i]} />
-                    <button on:click={() => onDelete(key, i)}>delete</button>
-                    {/each}
-                    <button on:click={() => onAdd(key)}>add</button>
-                </div>
-            {:else}
-                <input type="text" bind:value={obj[key]} />
-            {/if}
+            <svelte:self bind:obj={obj[i]} />
         </div>
+        <button on:click={() => onDelete(i)}>delete</button>
+        {/each}
+        <button on:click={() => onAdd(typeof obj[0])}>add</button>
     </div>
-{/each}
+{:else if typeof obj === "object"}
+    <div class="collection">
+        {#each Object.entries(obj) as [key, value]}
+        <div class="property">
+            <div class="key">{key}: </div>
+            <div class="value">
+                <svelte:self bind:obj={obj[key]}/>
+            </div>
+        </div>
+        {/each}
+    </div>
+{:else}
+    {String(obj)}
+{/if}
 
 <style>
     .property {
