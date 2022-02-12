6fb1511c8c13d9141a6caaa02152cfd338558034
Matyanson <matyas.perem@seznam.cz>
2022-02-13 00:41:55 +0100

    feat: import plan from text

--- src/components/PlanEditor.svelte
+++ src/components/PlanEditor.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import type ITimetable from "../models/Timetable";
-    import { timetables } from "../store";
+    import { plans, timetables } from "../store";
     import default_timetable from "../assets/default_timetable";
     import SubjectsContainer from "./SubjectsContainer.svelte";
     import Timetable from "./Timetable/index.svelte";
     import type { SubjectEvent } from "../models/Plan";
+    import Select from "./Select.svelte";
+    import type Plan from "../models/Plan";
 
 
     let selectedSubject = null;
@@ -12,6 +14,9 @@
     export let timetableTemplate: ITimetable = null;
     export let subjectEvents: SubjectEvent[] = [];
     let timetableList: {title: string, data: ITimetable}[];
+
+    let planString = '';
+    let errorMsg = '';
     $: timetableList = [{title: 'default', data: default_timetable}].concat($timetables);
     
     $: timetableTemplate = timetableList[timetableIndex].data;
@@ -38,21 +43,61 @@
             ...subjectEvents.slice(index + 1)
         ]
     }
+    const copy = () => {
+        const planData: Plan = {
+            timetable: timetableTemplate,
+            events: subjectEvents
+        };
+        const text = JSON.stringify(planData);
+        navigator.clipboard.writeText(text);
+    }
+    const importData = () => {
+        try {
+            errorMsg = '';
+            const data: Plan = JSON.parse(planString);
+            if(!data.events || !data.timetable) throw "";
+            
+            subjectEvents = data.events;
+            if(isDefaultTimetable(data.timetable)){
+                timetableIndex = 0;
+            } else {
+                timetableIndex = timetableList.length; //the future last index
+                timetables.update(old => [...old, { title: 'Imported timetable', data: data.timetable }]);
+            }
+
+            timetableTemplate = data.timetable;
+
+        } catch (e) {
+            errorMsg = "couldn't process the data";
+        }
+        planString = '';
+
+        function isDefaultTimetable(timetable: ITimetable) {
+            if(timetable.length !== default_timetable.length)
+                return false;
+            for(let i in timetable) {
+                const timesA = timetable[i];
+                const timesB = default_timetable[i];
+                if(timesA[0] !== timesB[0] || timesA[1] !== timesB[1])
+                    return false;
+            }
+            return true;
+        }
+    }
 </script>
 
 <!-- Select subject to place on the timetable -->
 <SubjectsContainer bind:selected={selectedSubject} />
 
 <!-- Select which Timetable to use -->
-<select bind:value={timetableIndex} >
-    {#each timetableList as template, i}
-        <option value={i}>
-            {template.title}
-        </option>
-    {/each}
-</select>
-{timetableIndex}
+<Select bind:value={timetableIndex} list={timetableList} titleKey={'title'} />
 <button on:click={() => subjectEvents = []}>clear all subjects</button>
+<button on:click={copy}>copy plan</button>
+<div>
+    <input id="importText" type="text" bind:value={planString} />
+    <button on:click={importData}>import from text</button>
+    <div class="error">{errorMsg}</div>
+</div>
 
 {#if timetableTemplate}
     <Timetable
@@ -63,5 +108,10 @@
 {/if}
 
 <style>
-
+    #importText {
+        max-width: 30em;
+    }
+    .error {
+        color: red;
+    }
 </style>
